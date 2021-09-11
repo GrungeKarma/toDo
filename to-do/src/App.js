30d5f6bbcdb3075ad17bbaa281ecd9f383adf50d
Anthony Lopez <grungekarma@gmail.com>
2021-09-11 15:48:36 -0500

    fix css and finish

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  //build a toDo app
-  // input for new task
+  //input that we can type new tasks into
   //button submit task and put into list of tasks
   const [taskList, setTaskList] = useState([]);
   const [inputTask, setInputTask] = useState('');
+  console.log(taskList);
 
   function handleSubmit(e) {
+    e.preventDefault();
 
     const newTask = [
       ...taskList,
@@ -18,10 +18,9 @@ function App() {
         taskName: inputTask,
         completed: false
       }
-
     ]
     setTaskList(newTask);
-    e.preventDefault();
+    setInputTask('');
   }
 
   const toggleCompleted = (clickedTaskId) => {
@@ -29,36 +28,33 @@ function App() {
       if (item.id === clickedTaskId) {
         return {
           ...item,
-          completed: item.completed
+          completed: !item.completed
         }
+      } else {
+        return item
       }
-      else {
-        return item;
-      }
-
     })
     setTaskList(newList);
   }
+
   const clearCompleted = () => {
     const clearSelected = taskList.filter(task => task.completed !== true);
     setTaskList(clearSelected);
   }
 
-  const clearAll = () => setTaskList([]);
+  // const clearAll = () => setTaskList([]);
 
 
 
   return (
     <div className="App">
-      <main className="App-Main">
-        <h1>Hello</h1>
-        <form action="">
+      <main className="App-main">
+        <form>
           <input
-            type="text"
-            placeholder="name task here"
+            type='text'
+            placeholder='name task here'
             onChange={(e) => setInputTask(e.target.value)}
             value={inputTask}
-
           />
           <button
             onClick={handleSubmit}
@@ -76,8 +72,9 @@ function App() {
             )
           })
         }
-        <button
-        ></button>
+
+        <button onClick={clearCompleted}>clear completed</button>
+        <button onClick={() => setTaskList([])}>clear all</button>
       </main>
     </div>
   );
